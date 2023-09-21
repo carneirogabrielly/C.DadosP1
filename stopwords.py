10bb8da9b3713a3efd1e2970dd77abec797ded37
alis-on-araujo <alisonao@al.insper.edu.br>
2023-09-21 11:31:48 -0300

    stopwords

---
+++ stopwords.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+stopwordsdic = stopwords.words('portuguese')
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('word_tokenize')
+
+def stopwords(texto):
+    
+    palavras = word_tokenize(texto, language='portuguese') # Tokenize é analisar palavras individualmente, basicamente
+    
+    palavras_sem_stopword = []
+    
+    for palavra in palavras:
+        if palavra not in stopwordsdic:
+            palavras_sem_stopword.append(palavra)
+    
+    # Reúna as palavras sem stopwords em uma string novamente
+    texto_sem_stopword = ' '.join(palavras_sem_stopword)
+    
+    return texto_sem_stopword
+
+ex = 'eu gosto muito de a minha maçã'
+ex_sem_stopwords = stopwords(ex)
+print(ex_sem_stopwords)
